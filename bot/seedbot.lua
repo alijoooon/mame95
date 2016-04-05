57046059650d1a456b13deb19188ede3a575a67a
alijoooon <djalijoooon@gmail.com>
2016-04-05 04:47:15 -0700

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,7 +226,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {110626080,103649648,143723991,111020322,0,tonumber(our_id)},--Sudo users
+    sudo_users = {204285160,126477037},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
@@ -324,7 +324,7 @@ This command will send text to [group_id]
 
 ]],
     help_text = [[
-Commands list :
+Commands list :85 bot ♥
 
 !kick [username|id]
 You can also do it by reply
